1808a6a95c2f3e58969802e1f183c7d8e83f1ac1
albseb511 <albertsebastian511@gmail.com>
2019-12-01 01:49:32 +0530

    cors

--- index.js
+++ index.js
@@ -4,6 +4,26 @@ var cors = require('cors')
 var server = require('http').createServer(app)
 var io = require('socket.io')(server,{ origins: '*:*'})
 port = process.env.port || 8000
+app.use(cors({ origin: '*' }));
+
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.header('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
 
 users = []
 connections = []
