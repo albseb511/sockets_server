44e81b4b918fa1cc6616cbcba269daa204ac5b49
albseb511 <albertsebastian511@gmail.com>
2019-12-01 09:21:31 +0530

    update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
+//
 var app = require("express")();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
 var leaderBoard = {};
 port = process.env.port || 8000;
-
 users = [];
 connections = [];
 gameRoom = { name: "GAME#1234", users: [] };
@@ -13,7 +13,6 @@ count = 0;
 var flag = false;
 var count = 0;
 var swiper;
-
 io.on("connection", socket => {
   socket.on("add user", data => {
     if (users.indexOf(data) >= 0) {
@@ -28,7 +27,6 @@ io.on("connection", socket => {
     console.log("connected: %s sockets connected", connections.length);
     io.emit("broadcast", { users: users, type: "newUser", game: gameRoom });
   });
-
   socket.on("add message", data => {
     console.log(data);
     if (data.message != "") {
@@ -36,19 +34,18 @@ io.on("connection", socket => {
       io.emit("broadcast", { ...data, type: "newMessage" });
     }
   });
-
   socket.on("addPlayer", data => {
     console.log("adding " + data + " to game room");
     gameRoom.users = [...gameRoom.users, data];
     console.log("new gameroom is", gameRoom);
     io.emit("broadcast", { type: "gameRoomUpdate", game: gameRoom });
-
     if (!flagTimerRunning) {
       if (users.length > 1) {
         var x = setInterval(() => {
           if (users.length <= 1 || timer == 0) {
             clearInterval(x);
             io.emit("broadcast", { type: "timer", timer: "GAME STARTING" });
+            // leaderBoard = {}
             timer = 5;
             flagTimerRunning = false;
             return;
@@ -61,7 +58,6 @@ io.on("connection", socket => {
         io.emit("broadcast", { type: "timer", timer: "WAITING FOR PLAYERS" });
     }
   });
-
   socket.on("removePlayer", data => {
     console.log("removing " + data + " from game room");
     gameRoom.users.splice(gameRoom.users.indexOf(data), 1);
@@ -71,7 +67,6 @@ io.on("connection", socket => {
       io.emit("broadcast", { type: "timer", timer: "WAITING FOR PLAYERS" });
   });
   // socket.off('removePlayer',()=>{})
-
   socket.on("disconnect", e => {
     var pos = connections.indexOf(socket);
     if (users[pos]) {
@@ -84,9 +79,8 @@ io.on("connection", socket => {
     // socket.removeAllListeners();
     console.log("Disconneted: %s sockets remaining", connections.length, e);
   });
-
   function myFunction() {
-    console.log("my function is being called", "flag value is =", flag);
+    // console.log("my function is being called", "flag value is =", flag);
     flag = true;
     swiper = setTimeout(() => {
       //   console.log("swiper is called, with flag", flag, "count is", count);
@@ -95,65 +89,49 @@ io.on("connection", socket => {
     }, 5000);
   }
   socket.on("start timer", () => {
-    console.log("start timer initiated from clientside");
+      console.log(count)
+    //   if(count==0)
+    //     leaderBoard={}
+    // console.log("start timer initiated from clientside");
     if (!flag) {
       //   console.log("flag is true, calling my function", flag);
       flag = true;
       myFunction();
     }
   });
-  socket.on("change", data => {
-    console.log("changing location to", data.location, "for", data.name);
-    socket.to(data.location).emit("broadcast", {
-      channel: data.location,
-      user: data.name,
-      msg: data.name + " has joined"
-    });
-  });
-
-  socket.on("disconnect", e => {
-    var pos = connections.indexOf(socket);
-    if (users[pos]) {
-      // io.emit('kickout',users[pos])
-      console.log("removing user", users[pos]);
-      users.splice(pos, 1);
-      console.log("new users", users);
-      connections.splice(pos, 1);
+  function questionEnd(corAns,leaderBoard) {
+    // console.log("question end running");
+    flag = false;
+    io.emit("broadcast", { type: "currentWinner", currentWinner: corAns });
+    io.emit("broadcast", { type: "leaderBoard", leaderBoard: leaderBoard });
+    clearTimeout(swiper);
+  }
+  socket.on("responseToServer", data => {
+    console.log(data);
+    console.log('the leaderboard data is',leaderBoard)
+    // console.log("receiving answer from user", data.uid);
+    flag = true;
+    let corAns = "No one";
+    if (data.ans === true) {
+      corAns = data.uid;
     }
-    // socket.removeAllListeners();
-    console.log("Disconneted: %s sockets remaining", connections.length, e);
+    console.log(corAns)
+    if (!(data.uid in leaderBoard)) {
+      leaderBoard[data.uid] = Number(data.ans);
+    } else {
+      leaderBoard[data.uid] += Number(data.ans);
+    }
+    // answers[data.uid].push(data);
+    console.log(leaderBoard);
+    if (flag === true) {
+      questionEnd(corAns,leaderBoard);
+    }
   });
-});
-
-function questionEnd(corAns) {
-  console.log("question end running");
-  flag = false;
-  io.emit("broadcast", { type: "currentWinner", currentWinner: corAns });
-  clearTimeout(swiper);
-}
-
-socket.on("responseToServer", data => {
-  console.log(data);
-  // console.log("receiving answer from user", data.uid);
-  flag = true;
-  let corAns = "No One";
-  if (data.ans === true) {
-    corAns = data.uid;
-  }
-  if (!(data.uid in leaderBoard)) {
-    leaderBoard[data.uid] = Number(data.ans);
-  } else {
-    leaderBoard[data.uid] += Number(data.ans);
-  }
-  // answers[data.uid].push(data);
-  console.log(leaderBoard);
-  if (flag === true) {
-    questionEnd(corAns);
-  }
-});
-
-socket.on("handleGameOver", data => {
-  io.emit("broadcast", { type: "leaderBoard", leaderBoard: leaderBoard });
+  socket.on("handleGameOver", data => {
+      console.log('sending info to client')
+    io.emit("broadcast", { type: "leaderBoard", leaderBoard: leaderBoard });
+    // leaderBoard = {}
+  });
 });
 server.listen(port, () => {
   console.log("server running at ", port);
